c6a771d072c1f442c527864d9e9367bae8df1fce
RameshtaVishwakarma <55827453+RameshtaVishwakarma@users.noreply.github.com>
2021-05-27 10:37:54 +0530

    Update Lomuto Partition.cpp

--- Sorting/Lomuto Partition.cpp
+++ Sorting/Lomuto Partition.cpp
@@ -5,9 +5,9 @@ partition are discussed in the hoares partition*/
 #include<bits/stdc++.h>
 using namespace std;
 //Pivot is always the last element of the array.
-void lomutoPartition(int arr[], int low, int high, int pivot)
+void lomutoPartition(int arr[], int low, int high)
 {
-    int i=low-1;
+    int i=low-1,pivot=high;
     for(int j=low;j<=high-1;j++)
     {
         if(arr[j]<=arr[pivot])
@@ -30,7 +30,7 @@ int main()
 {
     int arr[] = {10,80,30,90,40,50,70};
     int n = sizeof(arr)/sizeof(arr[0]);
-    lomutoPartition(arr, 0,n-1,n-1);
+    lomutoPartition(arr, 0,n-1);
     printArray(arr,n);
     return 0;
 }
