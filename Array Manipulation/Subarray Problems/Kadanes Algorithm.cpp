51c90c398b4023bc934c59b32bff09b87d8f4fd7
RameshtaVishwakarma <55827453+RameshtaVishwakarma@users.noreply.github.com>
2021-07-20 16:21:15 +0530

    Add files via upload

---
+++ Array Manipulation/Subarray Problems/Kadanes Algorithm.cpp
@@ -0,0 +1,47 @@
+/* Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum.
+
+Kadane’s Algorithm:
+Initialize:
+    max_so_far = INT_MIN
+    max_ending_here = 0
+
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+  (c) if(max_ending_here < 0)
+            max_ending_here = 0
+return max_so_far
+
+Basically the approach here is to calculate the prefix sum and one the prefix sum is zero set the prefix sum as 0 and begin with new prefix sum.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSubArraySum(int a[], int size)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+//Time complexity: O(n) Space complexity: O(1)
+
+/*Driver program to test maxSubArraySum*/
+int main()
+{
+    int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int n = sizeof(a)/sizeof(a[0]);
+    int max_sum = maxSubArraySum(a, n);
+    cout << "Maximum contiguous sum is " << max_sum;
+    return 0;
+}
