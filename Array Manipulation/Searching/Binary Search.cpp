e44874cb507ef8f80825846a8935e3a49f44f6cb
RameshtaVishwakarma <55827453+RameshtaVishwakarma@users.noreply.github.com>
2021-07-24 15:45:16 +0530

    Update Binary Search.cpp

--- Array Manipulation/Searching/Binary Search.cpp
+++ Array Manipulation/Searching/Binary Search.cpp
@@ -5,7 +5,7 @@ bool binarySearch(int arr[], int n, int x)
 {
     int low = 0, high = n-1;
     int mid = (low+high)/2;
-    while(high>low)
+    while(high>=low)
     {
         if(arr[mid] == x)
         {
@@ -29,6 +29,7 @@ int main()
     int arr[] = {5,3,9,7,1,8,6};
     int n = sizeof(arr)/sizeof(arr[0]);
     if(binarySearch(arr,n,7)){cout<<"Element Found"<<endl;}else{cout<<"Element not Found"<<endl;}
+    return 0;
 }
 
 //Killing Strangers HAHAHAHAHAHAHA
